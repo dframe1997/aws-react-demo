3660f389adaf80af9cb428be3d07b9e7d2b618f6
dframe1997 <43210899+dframe1997@users.noreply.github.com>
2023-02-20 17:00:06 +0000

    Updated to integrate the new API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://8s6wue4ha0.execute-api.us-east-1.amazonaws.com",
   import: "https://8s6wue4ha0.execute-api.us-east-1.amazonaws.com",
   bff: "https://8s6wue4ha0.execute-api.us-east-1.amazonaws.com",
-  cart: "https://8s6wue4ha0.execute-api.us-east-1.amazonaws.com"
+  cart: "https://8s6wue4ha0.execute-api.us-east-1.amazonaws.com",
 };
 
 export default API_PATHS;
