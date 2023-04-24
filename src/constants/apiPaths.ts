5753c8c0ddd42aa1296e9ad64f53fa71f93491f8
dframe1997 <43210899+dframe1997@users.noreply.github.com>
2023-04-24 15:44:34 +0100

    Update apiPaths.ts

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://8s6wue4ha0.execute-api.us-east-1.amazonaws.com",
   import: "https://n2b4dgzgki.execute-api.us-east-1.amazonaws.com",
   bff: "https://8s6wue4ha0.execute-api.us-east-1.amazonaws.com",
-  cart: "http://localhost:3003/dev/api", // https://41e7f66uv9.execute-api.us-east-1.amazonaws.com/dev
+  cart: "https://41e7f66uv9.execute-api.us-east-1.amazonaws.com/dev"
 };
 
 export default API_PATHS;
